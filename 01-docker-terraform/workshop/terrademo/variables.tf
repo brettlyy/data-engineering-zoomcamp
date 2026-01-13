b7b27c7e7553e478a2517f570730b56b87235476
Brettly <brettcd@icloud.com>
2026-01-13 12:29:59 -0800

    Work through terraform section with notes.

---
+++ 01-docker-terraform/workshop/terrademo/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  description = "Project"
+  default     = "terraform-demo-484217"
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "GCP Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "terraform-demo-484217-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
